33dbc2047298a7bba9f02b13c6ad5fde74d31d82
Naresh Kumar <banvro07@gmail.com>
2023-08-18 14:22:49 +0530

    python intro

---
+++ batch-august/intro.py
@@ -0,0 +1,272 @@
+# Lnaguage : python
+
+
+# editor : VScode  , pytcharm, anaconda, syder, jupyter......
+
+
+                            # Gudio Van Rossum
+# 1980 ------ 1991  
+
+# 1991 ----- python 0.9.0
+
+
+# 2000 --- python 2   =======> end  2020
+
+
+# 2008 --- python 3  .....>
+
+
+
+# unique features:
+    # 1) Simple
+    # 2) Gernal perpose
+    # 3) high lavel
+    # 4) Object oriented
+    # 5) dnymiclly typed
+    # 6) case senstive
+    # 7) Interpreted
+    # 8) Screpting langauge...
+
+# 1) Web devlpopment  
+            # django, flas
+
+# 2) MObile app backd
+    # DRF (Django rest freamework)
+
+# 3) Desk Top application
+        # pyqt5
+
+# 4) AI =>  
+
+
+
+
+# high lavel
+
+# languages:
+
+# high lavel  :  
+
+# low level  => machine language....    01
+
+# 3
+# 0011
+
+
+
+# Interpreted    vs   complier
+
+
+# Object oriented  oops: ()
+    # 1) Inharitance
+    # 2) Polymorhisum
+    # 3) Encapsulation
+    # 4) Abstraction
+
+
+# dnymiclly typed 
+
+    # a = 10
+
+    # int main(){
+    #     int a, b, c;
+    #     a = 10;
+    #     b = 20;
+    #     c = a + b
+    #     printf("%d", c)
+
+    # }
+
+    # a = 10
+    # b = 20
+    # c = a + b
+
+    # print(c)
+
+    # a = 10.9
+
+
+# case senstive : 
+
+    # age = 10
+
+    # Age = 20
+
+  # age = 10
+
+    # Age = 30
+
+    # print(age)
+
+# Screpting   
+    # follow top-down-approch
+
+
+# print("akjsdk")
+
+# print "asdbas"
+
+
+
+
+# print("Hello world..")
+
+
+# python keywords
+
+
+# int, float, def, class, len, sum, list, tuple, dict, .........
+
+# identifires  
+
+# abc = 10
+
+# Indentation
+
+
+# int main(){
+#     dfbkjsbdfkskdf
+#     sdjkfbksdf
+#     sdkjfbsdkfbdskjbfdsfbsdkfbdsfbkdf
+# }
+
+
+# def xua():
+#     kakaksbd
+#     asjdkasjdbasd
+#     askdkasdbaksd
+#     aksdjkasdb
+
+# print(asbd)
+
+
+
+# comments : use to increase code readibilty
+
+# 1) Single line comments
+
+# KDJFSDBFKSDJFBSKDFKKKBKkbkjbsdbfksdf
+
+# 2) Multiline comments
+# document interlude
+
+
+# """
+# dsjfbskdfbskdfjdks
+# fsdkjfbsdkbfksdf
+# sdjfbskdfjsdf
+# sdjfbksdfjbds
+# """
+
+
+
+# variables :   are use to hold data
+
+    # 1) Variables never sart from any integer and any spacial symbol
+
+    # 2) Variable always start from any charcter and underscore
+
+    # 3) Varibale naver contain space
+
+    # 4) Variable never be a keywrod
+
+
+# abc1 = 10000
+
+# 1abc = 1000
+
+# my_name = "kriss"
+# x = 100
+
+# _x = 200
+
+
+
+
+# PYTHON DATA TYPES : 
+    # 1) Interger
+    # 2) Float 
+    # 3) String 
+    # 4) Boolean
+    # 5) Complex 
+
+    # 6) Tuple 
+    # 7) List 
+    # 8) Set 
+    # 9) Dictionry
+
+
+# print()  => to print something in console
+# input()  => to get input from user.. 
+# len()  => to get length of elmenets
+# type()   => to check the type of a varibale
+
+# a = "kjsbksdbfksd fksd fk  12 "
+
+# a = True
+
+# complex  == >  a + bj
+# a = 1 + 3j
+
+# print(type(a))
+
+# a = 'aksjdkjasdbask'
+# b = 100
+# print("ajksdbaskdjbaksjdb : #", a, b)
+
+# a = "abc"
+
+# print(len(a))
+
+# input()  ===>   string
+
+# age = input("Enter your age : ")
+
+# print("User enterd : ", age)
+
+
+# when two strings are join, this process is called concatenation
+
+# adation of tw numbers :
+
+# a = 10
+# b = 20
+# c = a + b 
+
+# print(c)
+
+
+
+# a = int(input("ENter first number : "))
+# b = int(input("Enter second number : "))
+
+# c = a + b
+
+# print("The sum is : ", c)
+
+
+
+# Type casting
+
+# a = 10
+
+# print(a, type(a))
+
+# b = float(a)
+
+# print(b, type(b))
+
+
+
+# b = str(a)
+
+# print(b, type(b))
+# b = "10"
+
+
+
+
+
+
+
+
