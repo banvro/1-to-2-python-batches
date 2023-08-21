c40c25f48e14fdedf5dd959dc319bca85821a12b
Naresh Kumar <banvro07@gmail.com>
2023-08-21 14:09:10 +0530

    If else sntatements

---
+++ batch-august/codationl_statements.py
@@ -0,0 +1,109 @@
+# Control Statements : 
+
+# elif 
+# nested if else
+
+# a = 198
+
+# # 18 == 18
+# if a == 10:
+#     print("this is if statement...")
+ 
+# elif a == 20:
+#     print("i am 20")
+ 
+# elif a == 18:
+#     print("i am 19")
+
+# elif a == 18:
+#     print("nooooooo")
+   
+# else:
+#     print("This is else part")
+    
+
+# if age == 18:
+#     print("Wecome this is your firs voat")
+
+# elif age > 18:
+#     print("your are eligible for voat")
+
+# else:
+#     print("you are not eligble...")
+
+    
+# age = int(input("Enter your age : "))
+
+# if age >= 18:
+#     if age == 18:
+#         print("First voat")
+#     else:
+#         print("Eligible")
+
+# else:
+#     print("Your are not eligible")
+
+
+# Loops : ittreation
+
+
+# 1) For loop
+
+# lst = [12, 23, 34 , "hello", "data"]
+
+# print(type(lst))
+
+# print(len(lst))
+
+# range(start, end, increment)
+
+# start = 0
+# end = n - 1
+# increment = 1
+
+# lst = [12, 40, "hlo", "car", 100]
+
+# for i in lst:
+#     print(i)
+#     print("hllo")
+#     print("ok")
+#     print(i, i, i)
+
+# print("xyz")
+
+# ax = "akjbsdaksjdbkadj"
+
+# for ia in ax:
+#     print(ik)
+
+
+
+# int
+
+# print()
+
+
+
+# for j in range(9):
+#     # print(j)
+#     if j == 4:
+#         # break
+#         # continue
+#         pass
+    
+#     print(j)
+
+
+# for i in range(8):
+#     print(i)
+#     pass
+
+# print("ok")
+
+
+
+
+
+
+
+
---
+++ batch-august/intro2.py
@@ -0,0 +1,90 @@
+                        # python history ::
+
+# Guido van roosum
+
+# 1980   -------   1991
+
+# python 0.9.0
+
+# 2000 ------ python 2    ////     2020
+
+
+# 2008 ------ python 3
+
+# modulo 
+# unix
+# c
+# abc lanague
+
+
+
+# python features  
+    # 1) Simple
+    # 2) Gernal Perpose
+    # 3) High lavel
+    # 4) Interpreated
+    # 5) Object oriented
+    # 6) Dynmiclly typed
+    # 7) Case Senstive
+    # 8) Scripted langauge.....
+
+# 1) Web devlopment
+        # Django, Flask
+
+# 2) MObile app backend
+        # DRF ( Django rest framwork)   => API's
+
+# 3) Desktop Application
+        # PyQT5
+
+# 4) AI: 
+    # Machine Larning
+    #     supervised leaning
+    #         regression
+    #             simple regression
+    #             multipal regresssion
+    #             ploynomial regression
+    #         classification
+    #             binery classifiaction
+    #             multiclas clasicication
+        # Unsupervised leaning:
+            # K-means clustring
+        # renforcement leaning:
+        #     q-leaning
+
+
+# functions 
+# print()  => to print somthing on console 
+# input()  => to get input from user...
+
+# a = "akjsbdkasdkjasbdkas"
+# a = True
+
+# print(type(a))
+
+# a = 10
+
+# print(a, "ajsnaksdksadjbjk")
+
+
+# aff = 10
+
+
+
+# // 
+
+# skajbdkasjdkasdbksad
+
+"""
+lksdbsdf
+sdfbkdjfbks
+
+"""
+# /*kjsdksadkbasdk*/
+
+
+
+# list 
+
+
+
