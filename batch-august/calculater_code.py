42bccfeb36831ecc001abf76f12afca2041be6b3
Naresh Kumar <banvro07@gmail.com>
2023-08-22 14:05:49 +0530

    Calculeter

---
+++ batch-august/calculater_code.py
@@ -0,0 +1,144 @@
+# x = 10
+# y = 20
+
+# if x > 5:
+#     print("x is greater than 5")
+#     if y > 15:
+#         print("y is also greater than 15")
+#     else:
+#         print("y is not greater than 15")
+# else:
+#     print("x is not greater than 5")
+#     if y > 15:
+#         print("y is greater than 15")
+#     else:
+#         print("y is not greater than 15")
+
+# while loop
+
+# a = 10
+# b = 0
+
+# while b < a:
+#     b = b + 1
+#     print(b, "i am ok")
+
+
+
+
+# while True:
+#     choice = input("press q for quit and s for start : ")
+    
+#     if choice == "q":
+#         break
+    
+#     elif choice == "s":
+#         print("i am start noww")
+    
+#     else:
+#         print("something went wrong...")
+
+
+# if else elif
+# for loop
+# while lop
+
+
+# press q for quit and s for start calculater : 
+
+
+# s
+
+# **********calculater*****
+
+# num1 = 
+# num2 = 
+
+# "wnter what you want to do...."
+# 1) Adation
+# 2) Sutrction
+# 3) Multiplication
+# 4 Devsion
+
+# coice = 
+
+
+
+# print("""
+# askdbaksbdkasd
+# askdkbsadj
+# """)
+
+
+while True:
+    sq = input("Press q for quit and s for start : ")
+    
+    if sq == "q":
+        break
+
+    elif sq == "s":
+        print("**************** Calculater ***********")
+        
+        num1 = int(input("Enter first number : "))
+        num2 = int(input("ENter second number : "))
+        
+        print("""
+            Select one of themm...
+            
+            1) Adation
+            2) Substraction
+            3) Multiplication
+            4) Devsion
+        """)
+        
+        choice = input("ENter what you want to do : ")
+        
+        if choice == "1":
+            print("the sum is : ", num1 + num2)
+        
+        elif choice == "2":
+            print("the sub is : ", num1 - num2)
+        
+        elif choice == "3":
+            print("the multiplication is : ", num1 * num2)
+        
+        elif choice == "4":
+            print("the devision is : ", num1 / num2)
+        
+        else:
+            print("ENter something vaild")
+    
+    else:
+        print("Enter vaid word...")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ batch-august/loops.py
@@ -0,0 +1,47 @@
+# x = 10
+# y = 20
+# z = 30
+
+# if x > 5:
+#     print("x is greater than 5")
+    
+#     if y > 15:
+#         print("y is greater than 15")
+        
+#         if z > 25:
+#             print("z is greater than 25")
+#         else:
+#             print("z is not greater than 25")
+            
+#     else:
+#         print("y is not greater than 15")
+        
+#         if z > 25:
+#             print("z is greater than 25")
+#         else:
+#             print("z is not greater than 25")
+            
+# else:
+#     print("x is not greater than 5")
+    
+#     if y > 15:
+#         print("y is greater than 15")
+        
+#         if z > 25:
+#             print("z is greater than 25")
+#         else:
+#             print("z is not greater than 25")
+            
+#     else:
+#         print("y is not greater than 15")
+        
+#         if z > 25:
+#             print("z is greater than 25")
+#         else:
+#             print("z is not greater than 25")
+
+# a = 10
+# b = 0
+
+# while b < a:
+#     print("i am ok")
