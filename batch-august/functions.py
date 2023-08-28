37469c53f3ca35dfe91e73a7f84a06e85c5d0e5e
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-08-28 00:52:33 -0700

    Create functions.py

---
+++ batch-august/functions.py
@@ -0,0 +1,48 @@
+# Function : code optimization 
+
+# DRY : 
+
+# decleartion of functions
+
+# def xyz():
+#     print("this is a xyz function")
+    
+# call a function
+
+# xyz()
+
+# xyz()
+
+
+# xyz()
+
+
+# perameters
+# arguments
+
+# def sumthis(a, b, c):
+#     c = a + b + c
+#     print(c)
+
+# # arguents
+# sumthis(12, 10, 100)
+
+# sumthis(100, 200, 200)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
