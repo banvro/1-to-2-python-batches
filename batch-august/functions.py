765fec45005e6f2742e98ac98d4e7a7634b08e75
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-08-28 01:29:14 -0700

    Update functions.py

--- batch-august/functions.py
+++ batch-august/functions.py
@@ -31,18 +31,65 @@
 
 
 
+# for i in range(1, 11):
+#     print("2 x ", i, "=", 2 * i)
+
+# 2 x 1 = 2
+# 2 x 2 = 4
+# 2 x 3 = 6
+# .
+# .
+# .
+# .
+# .
+# 2 x 10 = 20
 
 
 
+# table(50)
 
 
 
+# def tablee(n):
+#     for i in range(1, 11):
+#         print(n, "x ", i, "=", n * i)
+        
+
+# tablee(6)
+
+# tablee(90) 
+
+# *args
+# **kwargs
 
 
+# def myinfo(name, age):
+#     print("my name is : ", name)
+#     print("my age is : ", age)
+    
+# myinfo(age = 20,name =  "moris")
 
 
+# default perameters
+
+# def sumthisnw(a = 100, b = 200):
+#     c = a + b
+#     print(c)
+
+# sumthisnw(1, 2)
 
 
+# *args
+# **kwargs
+
+# *args 
 
 
-
+def dothis(*abc):
+    print(abc)
+    z = 0
+    for i in abc:
+        z = z + i
+    print(z)
+    
+dothis(10, 20, 100)  
