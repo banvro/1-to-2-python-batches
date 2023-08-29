cbc806e58f0f22d80b5a6a8d0319d073a44da732
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-08-28 22:08:23 -0700

    Update lambda-function.py

--- batch-august/lambda-function.py
+++ batch-august/lambda-function.py
@@ -31,14 +31,45 @@
 # filter()
 # reduce()
 
-a = [12, 20, 100, 200, 90, 40]
+# a = [12, 20, 100, 200, 90, 40]
 
-# map()
-# map(function, itreater)
+# # map()
+# # map(function, itreater)
 
-qw = map(lambda x : x + 20, a)
+# qw = map(lambda x : x + 20, a)
 
-print(list(qw))
+# print(list(qw))
+
+# qw = [1, 2, 3, 4, 5, 7, 8]
+
+# zx = map(lambda x : x ** 2, qw)
+
+# print(list(zx))
+
+# filter 
+
+# a = [12, 23, 10, 90, 78, 100, 78, 3, 11]
+
+# # filter(function, itrater )
+
+# zx = filter(lambda x : x%2 != 0, a)
+
+# print(list(zx))
+
+
+from functools import reduce
+
+a = (1, 2, 3 ,4, 5, 6,7 , 8)
+
+zx = reduce(lambda a, b : a + b, a)
+
+print(zx)
+
+
+
+
+
+
 
 
 
