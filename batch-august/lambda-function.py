827bf7d640f8b7ba122e5d0af059141909d73601
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-08-28 21:40:36 -0700

    Create lambda-function.py

---
+++ batch-august/lambda-function.py
@@ -0,0 +1,44 @@
+# Functions 
+
+# lambda function
+
+# lambda arguments : expession
+
+
+# def sumthis(a, b):
+#     c = a + b
+#     print(c)
+
+# sumthis(12, 10)
+
+# zx = lambda a, b : a + b
+
+# print(zx(100, 200))
+
+# ey = lambda 
+
+# def sq(a):
+#     z = a ** 2
+#     print(z)
+    
+# sq(10)
+
+# asr = lambda a : a ** 2
+
+# print(asr(90))
+
+# map()
+# filter()
+# reduce()
+
+a = [12, 20, 100, 200, 90, 40]
+
+# map()
+# map(function, itreater)
+
+qw = map(lambda x : x + 20, a)
+
+print(list(qw))
+
+
+
