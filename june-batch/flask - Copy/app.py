5678c0b6ec74f5b6f43e821739ace3924719d411
Naresh Kumar <banvro07@gmail.com>
2023-09-08 01:06:49 -0700

    Save data

---
+++ june-batch/flask - Copy/app.py
@@ -0,0 +1,57 @@
+from flask import Flask
+from flask import render_template, request, redirect
+
+import mysql.connector
+
+
+conn = mysql.connector.connect(host = "localhost", username = "root", password = "1234", database = "xyz")
+
+curser = conn.cursor()
+
+
+
+
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def xyz():
+    return "hello website.."
+
+@app.route('/about')
+def aboutus():
+    return "this is my about page....."
+
+@app.route('/show/<int:id>')
+def showthis(id):
+    print(id)
+    # return render_template('abc.html')
+    return f"my id is {id}"
+
+
+@app.route('/xyz')
+def showthiss():
+    return render_template('myfrom.html')
+
+@app.route('/send_data', methods = ['post', ])
+def savedata():
+    if request.method == "POST":
+        uname = request.form.get('usernmae')
+        age = request.form.get('age')
+        # print(uname, age)
+        curser.execute(f"insert into mytable values('{uname}', {age})")
+        conn.commit()
+
+        return redirect("xyz")
+        # user = request.form['usernmae']
+
+        return f"usernae is {uname} and user age is {age}"
+
+    
+
+
+
+if __name__ == "__main__":
+    app.run(debug = True) 
