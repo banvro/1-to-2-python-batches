f9ad38ffd6cda6d6f1d1f7c6a58378576d580f26
Naresh Kumar <banvro07@gmail.com>
2023-09-19 01:37:00 -0700

    Data save queryies

--- june-batch/new-flask/app.py
+++ june-batch/new-flask/app.py
@@ -1,6 +1,12 @@
 # pip install flask
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import mysql.connector
+
+
+conn = mysql.connector.connect(host="localhost", username= "root", password="1234", database="flasktable")
+
+curser = conn.cursor()
 
 app = Flask(__name__)
 
@@ -13,6 +19,20 @@ def home():
 def contact():
     return render_template("contact.html")
 
+@app.route("/savedata", methods = ["post"])
+def savedata():
+    if request.method == "POST":
+        name = request.form.get("name")
+        number = request.form.get("number")
+        email = request.form.get("email")
+        dec = request.form.get("dec")
+
+        # print(name, number, email, dec)
+        curser.execute(f"insert into flasktable values('{name}', {number},'{email}', '{dec}')")
+
+        return f"save data {name, number, email, dec}"
+    return "save data"
+    
 @app.route("/about")
 def about():
     return render_template("about.html")
