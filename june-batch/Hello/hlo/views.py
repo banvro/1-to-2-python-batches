1c45bbed159b288db8aad445691fab65c22f13c4
Naresh Kumar <banvro07@gmail.com>
2023-10-05 01:16:27 -0700

    update done

--- june-batch/Hello/hlo/views.py
+++ june-batch/Hello/hlo/views.py
@@ -11,7 +11,28 @@ def home(request):
 
         savedata = ContactUs(Name = name, Number = number, Emial = email, Message = msg)
         savedata.save()
-    return render(request, "home.html")
+    return render(request, "home.html", {"show" : "home"})
+
+def update(request, id):
+    data = ContactUs.objects.get(id = id)
+    return render(request, "home.html", {"show" : "update", "mydata" : data})
+
+
+def updatethis(request, id):
+    data = ContactUs.objects.get(id = id)
+    if request.method == "POST":
+        print("iiiiiiiiiiiii")
+        name = request.POST.get("name")
+        number = request.POST.get("number")
+        email = request.POST.get("emial")
+        msg = request.POST.get("msg")
+
+        data.Name = name
+        data.Number = number 
+        data.Emial = email
+        data.Message = msg 
+        data.save()
+    return redirect("/")
 
 
 
@@ -24,3 +45,4 @@ def deletme(request, id):
     data = ContactUs.objects.get(id = id)
     data.delete()
     return redirect("showdata")
+
