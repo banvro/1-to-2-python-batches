f45fd0aaeca4859a5b838d2aaafa2de705075c92
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-10-12 01:26:34 -0700

    Update urls.py

--- drf-apis/myapis/allapis/urls.py
+++ drf-apis/myapis/allapis/urls.py
@@ -4,4 +4,6 @@ from allapis import views
 urlpatterns = [
     path("showsomthing", views.showthis),
     path("all-conect-us-records", views.allcontact),
+    path("save-conect-us-records", views.savecontactus),
+    path("update-conect-us-records/<int:id>", views.updatedata),
 ]
