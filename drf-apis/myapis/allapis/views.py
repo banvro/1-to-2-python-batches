642e3205b603ec1120afcef09831f05a455110d7
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-10-12 01:26:14 -0700

    Update views.py

--- drf-apis/myapis/allapis/views.py
+++ drf-apis/myapis/allapis/views.py
@@ -28,3 +28,22 @@ def allcontact(request):
     
     except Exception as e:
         return Response({"Error" : "Something Went Wrong"})
+    
+@api_view(["POST"])
+def savecontactus(request):
+    print(request.data)
+    savemydata = ContactSerlizer(data = request.data)
+    if savemydata.is_valid():
+        savemydata.save()
+    return Response({"data" : "Data saved sucessfully..",
+                     "Data" : savemydata.data})
+
+@api_view(["PATCH"])
+def updatedata(request, id):
+    obj = ContactUs.objects.get(id = id)
+    seri = ContactSerlizer(obj, data = request.data, partial = True)
+    if seri.is_valid():
+        seri.save()
+    return Response({"data" : "Data Update sucessfully..",
+                     "user id" : id,
+                     "mydata" : seri.data})
