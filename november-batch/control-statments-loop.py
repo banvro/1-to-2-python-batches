0a7c56fbe791c7dbf085508b7ccb488b34069456
Naresh Kumar <banvro07@gmail.com>
2023-11-15 02:15:58 -0800

    python if else elif.py

---
+++ november-batch/control-statments-loop.py
@@ -0,0 +1,153 @@
+# Control Statments : are those statments, which control the flow of execution of a program.
+
+# Condational Statments:
+    # if elif else
+    
+
+# comparission operator
+
+# 1) ==
+
+# print(11 == 10)
+
+# 2) >
+# 3) <
+# 4) <=
+# 5) >=
+
+# print(10 < 4)
+
+
+# 10 + 20
+
+# operands = 10, 20
+# operator = +
+
+
+
+
+
+# if elif else
+
+# elif = else if
+# Voating system
+
+# age = 19
+
+# if condation:
+#     # block of code   , it execute only if condation is true
+
+# age = 19
+
+
+# 19 > 18
+# if age > 18:
+#     print("you are eligible for voat.")
+  
+# elif age == 18:
+#     print("this is your first voat")
+
+# elif age == 19:
+#     print("this is seocnd voat")
+
+# else:
+#     # execute only when if condation is false
+#     print("you are not eligiible for voat")
+
+
+
+# Nested if else:
+
+
+
+# age = 17
+# # 20 > 18
+# if age > 18:
+#     # 20 == 18
+#     if age == 18:
+#         if age == 16:
+#             print("no")
+#         else:
+#             print("this is ok")
+    
+#     elif age == 17:
+#         print("this is not ok")
+
+# elif age < 17:
+#     if age == 17:
+#         print("seventeen")
+#     else:
+#         print("done")
+
+# else:
+#     print("hellooooo")
+
+
+# print(10 == 11)
+
+
+
+# num = 21
+
+# if num % 2 == 0:
+#     print("this is devisible by 2")
+
+
+# print(3 % 2)
+
+
+# Write a Python program that iterates the integers from 1 to 50. For multiples of three print "Fizz" instead of the number and for multiples of five print "Buzz". For numbers that are multiples of three and five, print "FizzBuzz".
+
+
+
+
+
+
+
+
+# Loops : loops are used for ittrations/ repetation.
+
+# 1) For Loop
+# 2) While Loop
+
+# 1) For loop : 
+
+
+# lst = [10, 20, 89, 7, 8, 6, 10]
+
+# for varible in data_source:
+#     # block of code
+
+# for a in lst:
+#     print(a)
+#     print("hello.....")
+
+
+
+# range()
+
+# range(start, end, increment):
+
+# start = 0
+# end = n - 1
+# increment = 1
+
+# for i in range(3, 15):
+#     print(i)
+
+
+
+
+# 2 x 1 = 2
+# 2 x 2 = 4
+# 2 x 3 = 6
+# .
+# .
+# .
+# .
+# 2 x 10 = 20
+
+
+
+for i in range(1, 11):
+    print("2 x", i, "=", 2*i)
