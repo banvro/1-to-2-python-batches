329e8f10cece2fa2099e7a999405acbf233be159
Naresh Kumar <banvro07@gmail.com>
2023-11-15 02:14:41 -0800

    python intro 2

---
+++ november-batch/control-statments.py
@@ -0,0 +1,135 @@
+class code:
+# control statments:
+
+# condational statments:
+
+# if elif else
+
+
+# print(11==10)
+
+
+# Voating system:
+
+
+# age = 12
+# #   19 > 18
+# if age > 18:
+#     print("you are eligibal for voat")
+
+# elif age == 18:
+#     print("welcome this is your first voat")
+
+# elif age == 12:
+#     print("this is your seond voat")
+
+# else:
+#     print("you are not eligibl for voT")
+
+
+# Loop:
+#     1) For Loop
+#     2) While Loop
+
+# a = "this is a car"
+
+# print(len(a))
+
+# for varibale in ittrable objact
+# a = "this is a car"
+
+
+
+# for i in a:
+#     print(i)
+#     print("hello world")
+#     print(i, i, i)
+#     print("ok")
+
+
+
+# a = [10, 20, 78, 90, 6, 56]
+
+
+# for x in a:
+#     print(x)
+#     print("hello ")
+
+
+# print("as dasd aaksjdbkasdbkad")
+
+
+
+# range(start, end, increment) 
+
+# start = 0
+# end = n - 1
+# increment = 1
+
+
+# for a in range(1, 11, 1):
+#     if a == 2:
+#         print("hello")
+#     print(a)
+
+
+# 2 x 1 = 2
+# 2 x 2 = 4
+# 2 x 3 = 6
+# 2 x 4 = 8
+# .
+# .
+# .
+# 2 x 10 = 20
+
+
+
+# print("jejhd")
+
+# range(1, 101)
+
+# a = [12, 10, 10]
+
+# start = 0
+# end = n - 1
+# increment = 1
+
+# for i in range(10, 101, 5):
+#     print(i)
+
+
+# for i in range(1, 11):
+#     print("hellooo")
+
+# for a in range(1, 11):
+#     # print("2 x", a, "=", 2*a)
+#     print(f"2 x {a} = {2*a}")
+
+
+
+
+
+
+# FizzBuzz Problem: Write a Python program that prints the numbers from 1 to 100. But for multiples of 3, print "Fizz" instead of the number, and for the multiples of 5, print "Buzz." For numbers that are multiples of both 3 and 5, print "FizzBuzz."
+
+
+# for i in range(1, 101):
+#     if i % 5 ==0 and i % 3 == 0:
+#         print("FizzBizz")
+        
+#     elif i % 3 == 0:
+#         print("Fizz")
+    
+#     elif i % 5 == 0:
+#         print("Buzz")
+   
+#     else:            
+#         print(i)
+
+
+2 x 12 = 2
+2 x 13 = 4
+2 x 14 = 6
+..
+..
+.
---
+++ november-batch/intro-2.py
@@ -0,0 +1,138 @@
+# AI : 
+
+
+# Languages:
+#     1) High level Language
+    
+#     2) Low level Languages / binary language/ machine language
+    
+#     01
+
+# compiler / interpreater
+
+
+# Numpy
+
+
+# Scripting:
+
+# a = 10
+# b = 20
+# c = a + b
+# print(c)
+
+
+# OOP's
+
+# 7) Dynamically typed:
+
+# int a;
+# a = 10
+
+
+# v = "thi isadfas"
+
+# 8) case senstive : 
+
+# a = 20
+
+# A = 30
+
+# age = 10
+
+# Age = "hello"
+
+
+
+
+# kjasbfkjbfkajbka
+
+
+# """
+#     sjdfbsbfsdjf
+#     sdhfsdhjfdvfjsdf
+#     sdhfvjsdfvhdsjfdsf
+#     dsfdshjfvdsjf
+
+# """
+
+# built in funcation
+
+
+# hleo()
+
+
+# print() : to print something on console
+
+# a = 10
+
+# print("skdfbkjdsbfksd a", a, "this os a jasdvb")
+
+# type() : use to know , what types of data a variable hold.
+
+# a + bj
+# a = 10 + 20j
+# ''
+# ""
+# """"""
+
+# print(type(a))
+
+# 1) List
+# 2) Tuple
+# 3) Set
+# 4) Dictionry
+
+# {key : value}
+
+# a = 10
+
+# zx = {12, 10, 89, "hlo"}
+
+# print(type(zx))
+
+
+
+# input() : 
+# count()
+# len()
+
+# a = "tahis 3a is a car"
+
+# print(a.count("a"))
+
+# str()
+
+# a = input()
+# b = input()
+
+# c = a + b
+
+# print(c)
+
+# concatenation : joining two string
+
+# a = "10"
+# b = "20"
+
+# print(a + b)
+
+
+# type casting : to convert data from one type to another is called types casting.
+
+# a = 10
+
+# print(a, type(a))
+
+# b = float(a)
+
+# print(b, type(b))
+
+
+
+a = int(input("enter first number : "))
+b = int(input("ENter second Number : "))
+
+c = a + b
+
+print(c)
