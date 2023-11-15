68b77b69312bcc15712c0103bf48c545f76863d7
Naresh Kumar <banvro07@gmail.com>
2023-11-15 02:10:42 -0800

    intro 2nd lecture.

---
+++ november-batch/python-introduction.py
@@ -0,0 +1,184 @@
+# print("hello world")
+
+# variables : 
+
+# 1) variables always atrt form charcters or undersocer
+# 2) varibale never be sart from any integer.
+# 3) variable never contaion any spaces or nay spacial symbols.
+# 4) varibles never be keyword
+
+
+# age12 = 20
+
+
+# my_name!@@#@#$#$%$^%$^ = "moris"
+
+# keyword : int, def, class, float, str
+
+# int = 20
+
+# identifires : 
+
+# age = 20
+
+
+
+# int main(){
+#     kasbdkbasd
+#     askjdbaksdad
+#     asdaskdsakdsadsadbksad
+# }
+
+
+# if 10==10:
+#     print("hello")
+
+
+# comments : to increase code readability
+
+
+# 1) single line comment
+
+# # aksjbdkasjbdasdbkasbdjasd
+
+# # 2) multiline comments
+
+# # print("helloooooo")
+
+# """
+# ksjdbksdjfsd
+# fsdjfbsdjbfksdbfkjsdkfbdksfjbdjbjsdbfksf
+
+# ""
+
+
+# Data Types:
+#     1) Integer 1, 2, 3, 0, 12, 
+#     2) Float  10.1, 10.0, 45.7, 90.8
+#     3) Boolean  True, False
+#     4) String
+#     'ths is a string'
+#     "this is also a string"
+#     """tis u alskjsbsajd """
+    
+#     5) Complex : a + bj
+#                     12 + 20j
+    # 6) list
+    # 7) tuple
+    # 8) set
+    # 9) dictionry
+
+
+# Python Built in funcation:
+
+# xyz()
+
+# 1) print() : to print anything on consloe.
+# 2) input()
+# 3) type() : use to get type of data
+# 4) len()
+# 5) count()
+
+# age = 100
+
+# print("this is print funcation ", age, "this is m age")
+
+# f-string
+
+# print(f"the {age} is my age")
+
+
+# a = """ajshja sdsa duasv das6weuwqvdajdv"""
+
+
+# a = 12 + 10j
+
+# print(type(a))
+
+
+# a = 10
+# b = 120
+
+# c = a + b
+
+# print("the sum is : ",  c)
+
+
+
+# Type casting : 
+
+
+# a = 10
+
+# print(a, type(a))
+
+# # int()
+# # float()
+# # str()
+
+# b = float(a)
+
+# print(b, type(b))
+
+
+
+# a = "12983"
+
+# print(a, type(a))
+
+# b = int(a)
+
+# print(b, type(b))
+
+
+
+# 3) input() => use to get input from user
+
+
+# a = 100
+# b = 200
+# c = a + b
+# print(c)
+
+
+# user = input("Enter your name : ")
+
+# print("user entered : ", user)
+
+# input() => str
+
+# concatenation : joining two string
+
+# num1 = input("Enter first number : ")
+# num2 = input("Enter seocnd number : ")
+
+# c = num1 + num2
+
+# print("the sum is : ", c)
+
+# a = "hello "
+# b = "ok"
+
+# c = a + b
+# print(c)
+
+# num1 = int(input("Enter first number : "))
+# num2 = int(input("Enter seocnd number : "))
+
+# c = num1 + num2
+
+# print("the sum is : ", c)
+
+
+# a = "asdkhsadj"
+
+
+
+
+# a = int(input("Enter your name : "))
+
+
+# print(a, type(a))
+
+
++, -, \, *
