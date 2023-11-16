3dd070b328b0e044716e44d00bf4c4660c1bf81e
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-11-16 02:22:10 -0800

    Create while-loop.py

---
+++ november-batch/while-loop.py
@@ -0,0 +1,143 @@
+# Loops:
+#     1) For Loop
+#     2) While Loop
+
+# 2) While Loop : 
+
+
+# while condation:
+    #code of block
+    
+
+# for i in range(1, 11):
+#     print(i)
+
+
+# while 10 == 10:
+#     print("this is a car")
+
+# a = 0
+
+# # 2 < 10
+# while a < 10:
+#     print(a)
+#     a = a + 1
+
+
+# 1) break
+# 2) continue
+# 3) pass
+# 4) assert
+
+# for i in range(1, 10):
+#     if i == 4:
+#         # break
+#         continue
+#     print(i)
+
+
+
+
+# a = 1
+
+# while a < 10:
+#     if a == 7:
+#         break
+#     print(a)
+#     a = a + 1
+
+
+# lst = [1, 4, 4, 2, 3, 5, 6, 2, 4, 6, 8, 10]
+
+# for i in lst:
+#     if i == 4:
+#         continue
+#     print(i)
+
+
+# for i in range(1, 10):
+#     pass
+
+
+# assert
+
+# assert 10 == 11, "this is a error"
+
+
+
+# 7 x 1 = 7
+# 7 x 2 = 14
+# .
+# .
+# .
+# .
+
+# 7 x 10 = 70
+
+# 10
+# 9
+# 8
+# 7
+# 6
+# 5
+# 4
+# 3
+# 2
+# 1
+
+
+
+
+# 7 x 1 = 7
+# 7 x 2 = 14
+# .
+# .
+# .
+# .
+
+# 7 x 10 = 70
+
+# for i in range(1, 11):
+#     print(f"7 x {i} = {7*i}")
+
+# a = 1
+
+# while a < 11:
+#     print(f"7 x {a} = {7*a}")
+#     a = a + 1
+
+
+# a = 10
+
+# while a > 0:
+#     print(a)
+#     a = a - 1
+
+
+
+# lst = [12, 23, 4, 3, 4, 2, 1, 13, 5, 6, 23, 100, 32]
+
+
+
+
+# a = "this is a car"
+
+# a = 239846932462342
+
+# a = 3241
+
+# 10
+
+
+
+a = 2374293462394
+
+b = str(a)
+
+total = 0
+
+for i in b:
+  
+    total = total + int(i)
+
+print(total)
