f40e8c8ca0a5d68a688a669ab2d0ce8960967477
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-11-17 00:37:07 -0800

    Create for-while-whileTrue.py

---
+++ november-batch/for-while-whileTrue.py
@@ -0,0 +1,93 @@
+# Loops: ittrations
+
+# a = 203423948
+# a = "u873tgweiuqeiq"
+
+    # for loop : is use to ittrate the ittrable sequence of data. where we know from where it starts and ending point.
+
+    
+    # while loop : is the condation based loop, where loop executes till condation is True.
+    
+# for variable in sequence:
+
+
+# a = 2983642934629
+
+# for i in a:
+#     print("helo", i)
+
+# range(strat, end , increment)
+
+# 10
+# 9
+# 8
+# 7
+# 6
+# 5
+
+# for i in range(10, 0, -1):
+#     print(i)
+
+
+
+# while loop:
+
+
+
+
+
+
+
+# while condation:
+#     kjsdkad
+
+
+# while True:
+    
+# a = 10
+
+# while a > 0:
+#     a = a - 1
+#     print("heloooooo", a)
+   
+
+
+# while True:
+
+
+
+
+# while True:
+#     inp = input("Press q for quit and s for start : ")
+    
+#     if inp == "q":
+#         break
+    
+#     elif inp == "s":
+#         print("""game start
+#         it is starting...""")
+    
+#     else:
+#         print("please enter somehtig valid")
+
+
+# a = 1232 
+
+# b = str(a)
+
+# # print(b[1])
+
+# a = 0
+# totle = 0
+
+# # 2 < 4
+# while a < len(b):
+#             # 1 + 2
+#     totle = totle + int(b[a]) #1, 3
+#     a = a + 1 # 2
+
+# print(totle)
+
+
+a = "012973012312031039203139"
+
