c4b0f46bbeaff4bba1929808c956da7329f0cf1b
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-12-11 03:03:19 -0800

    Create start-oops.py

---
+++ november-batch/start-oops.py
@@ -0,0 +1,83 @@
+# OOPs : 
+
+# 1) Class
+# 2) Objects
+# 3) Self
+# 4) Methods
+# 5) Constructer
+# 6) Attributes
+# 7) Inheritance:
+#     1) Single Level Inheritance
+#     2) Multiple Inhertiance
+#     3) Multilevel Inheritance
+#     4) Hireriachical Inheritance
+#     5) Hybrid INheritance
+
+# 8) Abstraction:
+#     1) Abstract Class
+#     2) Abstract Method
+#     3) Interface
+
+# 9) Encapsulation:
+#     Access Spacifires:
+#         1) Public Members
+#         2) Private Members
+#         3) Protected Members
+    
+# 10) Polymorphisum:
+#     1) Run time Polymorphisum
+#         1) Opertor Overloading
+#         2) Method Overlaoding
+        
+#     2) Compile Time Polymorphisum
+#         1) Method Overriding
+    
+#     Duck typing
+    
+    
+# Class : Class is a blueprint of an object.
+# object : it is an instance of a class.
+
+# self : 
+
+# class myclass:
+#     def car(self):
+#         print("this is a car")
+    
+#     def bus(self):
+#         print("this is a bus")
+    
+#     def train(self):
+#         print("this is a train")
+
+# obj = myclass()
+# obj.bus()
+# obj.car()
+# obj.train()
+
+# xyz = myclass()
+# xyz.train()
+
+
+
+# 4) Constructer : is a spicial method in a class, that can be make by using __init__. it call automatically while n object is created of that class.
+
+
+
+class cls1:
+    def __init__(self):
+        print("i am constructer")
+    
+    def car(self):
+        print("this is a cra")
+    
+
+ob = cls1()
+
+ob.car()
+
+
+
+
+
+
