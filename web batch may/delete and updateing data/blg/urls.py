c95c7a4b783efbc18b96eda85f42eb08f719eeb6
Naresh <banvro07@gmail.com>
2024-05-16 14:10:47 +0530

    delted daya

---
+++ web batch may/delete and updateing data/blg/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from blg import views
+
+urlpatterns = [
+    path("", views.homepage),
+    path("about", views.aboutus),
+    path("contact", views.contactus),
+    path("services", views.services),
+    path("save-data", views.savethinginfo),
+    path("delete-this/<int:xyz>", views.deletethis)
+]
