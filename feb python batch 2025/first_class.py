183ac29b086a037f1dbd6103836253f74d3513c4
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2025-02-11 13:53:04 +0530

    Add files via upload

---
+++ feb python batch 2025/first_class.py
@@ -0,0 +1,163 @@
+# # Python --->
+
+
+# # Comments :: increse code readability 
+
+
+# Data types:
+    
+#     Data ---> 
+    
+# 1) Integer
+#     123, 34, -324, 123
+
+
+# 2) Float
+#     10.6, 23.5
+    
+# 3) String
+#     'this is a string'
+#     "this is also a string"
+#     """this is also a string"""
+#     "10"
+
+# 4) Boolean
+#         True, False
+
+# 5) Complex
+#         a + bj --> 10 + 20j
+
+
+
+# # Array, -------------
+
+# 6) List
+#     [12, 34, 45, "hello", "no", 10.5]
+
+# 7) tuple (12, 34, 45, 56, 67,78)
+# 8) set  {12, 34, 5, 67, 78, 90}
+# 9) dictionry {key : value}
+    
+    
+# Indentifires:
+    
+#     Name --> Veriable, function name, class name, 
+    
+# Rules:
+    
+#     1) always start from charcter or underscore (_).
+#     2) never statrt from any integer.
+#     3) never hold spaces
+#     4) never hold any spacial symbols @#@$#%^&*()^)(*^&%)
+#     5) never be a keyword.
+    
+
+# keyword :::: are the spacial words. that are reserved by a language for there spacial use.
+
+# int, float, def, class, str, try, ,,........
+    
+
+# Veriable : 
+#         ---> are the containers use to hold data for fututre use.
+
+# abc = 20
+
+# x = 200
+
+# _x = 20
+
+# 12abc = 2999
+
+# 4ajksd =3294
+
+# a1 = 100
+
+# full name = "hellooo"
+
+# full_name = "helloooo"
+
+# xy!~@@# = 92834
+
+
+
+# int = 10
+
+# def = 20
+
+
+# Functions:
+#     1) Built in functions:
+#         --> pre define
+    
+#     2) user define function
+            
+
+
+# 1) print()
+# 2) input()
+# 3) type()
+# 4) len()
+# 5) count()
+
+
+# x()
+
+
+# 1) print() :::
+#     --> help to show anything on console. 
+
+# print("helow world")
+
+# x = 1000
+
+# print(x)
+
+# print("hello", x, "no", x, x)
+
+
+# zx = 2
+
+# pq = 3
+
+# x = zx + pq
+
+# print(zx, pq, x, sep = "-")
+
+
+
+# name = "kriss"
+
+# age = 20
+
+# phone_number = 9823482364
+
+
+# print("User name is ", name, "the age is ", age, "and number is ", phone_number)
+
+# f-string ---> 
+
+# print(f"user name is {name} and user age is {age} and user phone number is {phone_number}")
+
+
+# type()
+# -----> use to know what type of data a veriable hold..
+
+x = {12, 34, 45, 45}
+
+print(type(x))
+
+
+
+
+1) Simple
+2) Gernal Purpose
+3) High level
+4) Object oriented
+5) Scripting
+6) Interpreted
+7) Dynmiacally typed
+8) Case senstive
+
+8219836118
+
+
